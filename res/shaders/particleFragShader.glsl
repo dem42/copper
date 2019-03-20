db22714a19c635c94b0ba9c232f3fb5f9d4c06bb
martin <martin.papanek@gmail.com>
2019-03-20 03:36:03 +0100

    forgot the empty shaders

---
+++ res/shaders/particleFragShader.glsl
@@ -0,0 +1,7 @@
+#version 400 core
+
+out vec4 out_color;
+
+void main(void) {
+    out_color = vec4(1, 0, 0, 1);
+}
---
+++ res/shaders/particleVertShader.glsl
@@ -0,0 +1,5 @@
+#version 400 core
+
+void main(void) {
+    gl_Position = vec4(0, 0, 0, 1);
+}
