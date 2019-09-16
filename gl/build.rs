c4f89d7ed7c3cb7c7d508398d38d1ba64794b68d
martin <martin.papanek@gmail.com>
2019-09-16 23:12:06 +0200

    adding glfw as submodule so that we can build it from build.rs and it works in CI

--- gl/build.rs
+++ gl/build.rs
@@ -1,9 +1,9 @@
-extern crate gl_generator;
-
 use gl_generator::{Registry, Api, Profile, Fallbacks, GlobalGenerator};
 use std::env;
 use std::fs::File;
 use std::path::Path;
+
+use cmake;
 
 fn main() {
     let dest = env::var("OUT_DIR").unwrap();
@@ -13,7 +13,11 @@ fn main() {
         .write_bindings(GlobalGenerator, &mut file)
         .unwrap();
 
+    let dst = cmake::build("libglfw");
+
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    //println!("cargo:rustc-link-lib=static=foo");
 
     // link with glfw3 which is our dependency (the link library name seems to be specified inside glfw-rs which is a crate we depend on)
-    println!(r"cargo:rustc-link-search=D:\opengl_workspace\glfw-3.3.bin.WIN64\lib-vc2015"); 
+    //println!(r"cargo:rustc-link-search=D:\opengl_workspace\glfw-3.3.bin.WIN64\lib-vc2015"); 
 }
