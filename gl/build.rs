b1292371eb02c9c2c720b8cb9a927c28c7fd765d
martin <martin.papanek@gmail.com>
2019-09-17 14:19:43 +0200

    attempting to get the build working with compiled glfw

--- gl/build.rs
+++ gl/build.rs
@@ -6,7 +6,7 @@ use std::path::Path;
 use cmake;
 
 fn main() {
-    let dest = env::var("OUT_DIR").unwrap();
+    let dest = env::var("OUT_DIR").unwrap();    
     let mut file = File::create(&Path::new(&dest).join("gl_bindings.rs")).unwrap();
 
     Registry::new(Api::Gl, (4, 3), Profile::Core, Fallbacks::All, ["GL_EXT_texture_filter_anisotropic"])
@@ -15,7 +15,8 @@ fn main() {
 
     let dst = cmake::build("libglfw");
 
-    println!("cargo:rustc-link-search=native={}", dst.display());
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    eprintln!("Compiling glfw into: {}", dst.display());
     //println!("cargo:rustc-link-lib=static=foo");
 
     // link with glfw3 which is our dependency (the link library name seems to be specified inside glfw-rs which is a crate we depend on)
